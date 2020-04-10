68860ff882e9cc422a01f0f1f1601c9ccdc554f4
sap2me <sapronov.andrey.k@gmail.com>
2020-04-10 19:40:38 +0300

    Added analitics and post api calls

--- social_network/app/api.py
+++ social_network/app/api.py
@@ -4,6 +4,7 @@ from app import db
 from app.models import User, Post
 from app.tools import auth_required, json_response
 
+from copy import deepcopy
 import datetime
 
 api = Blueprint('api', __name__)
@@ -65,3 +66,83 @@ def unlike_post(user):
 	db.session.commit()
 	
 	return json_response("Post unliked")
+
+@api.route('/posts', methods=['GET'])
+def get_post():
+	""" Get all posts """
+	date_from = request.args.get('date_from')
+	date_to = request.args.get('date_to')
+	posts = Post.query
+	if date_from:
+		date_from = datetime.datetime.fromisoformat(date_from)
+		posts = posts.filter(Post.creation_time >= date_from)
+	if date_to:
+		date_to = datetime.datetime.fromisoformat(date_to)
+		posts = posts.filter(Post.creation_time <= date_to)
+
+	posts_list = []
+	for post in posts:
+		posts_list.append({
+			"text": post.text,
+			"author": post.author.name,
+			"likes_amount": post.likes_amount,
+			"users_likes": [user.name for user in post.user_likes],
+			"creation_time": post.creation_time
+		})
+	return jsonify(posts_list)
+
+@api.route('/analitics/', methods=['GET'])
+def analitics():
+	""" Analitics about single/all posts """
+	date_from = request.args.get('date_from')
+	date_to = request.args.get('date_to')
+	sort_by = request.args.get('sort_by')
+	sort_typies = ['year', 'month', 'day', 'hour', 'minute']
+	if not sort_by in sort_typies:
+		sort_by = 'day'
+	posts = Post.query.order_by(Post.creation_time.desc())
+	if date_from:
+		date_from = datetime.datetime.fromisoformat(date_from)
+		posts = posts.filter(Post.creation_time >= date_from)
+	if date_to:
+		date_to = datetime.datetime.fromisoformat(date_to)
+		posts = posts.filter(Post.creation_time <= date_to)
+
+	first_date = posts.first().creation_time
+	last_day = getattr(first_date, sort_by)
+	days_list = []
+	day_list = []
+	likes = 0
+	for ind, post in enumerate(posts):
+		current_day = getattr(post.creation_time, sort_by)
+		if current_day != last_day or ind == posts.count() - 1:
+			days_list.append({
+				"posts": deepcopy(day_list),
+				"likes": likes
+			})
+			day_list.clear()
+			likes = 0
+		likes += post.likes_amount
+		day_list.append({
+			"text": post.text,
+			"author": post.author.name,
+			"likes_amount": post.likes_amount,
+			"users_likes": [user.name for user in post.user_likes],
+			"creation_time": post.creation_time
+		})
+		last_day = current_day
+
+	return jsonify(days_list)
+
+@api.route('/users/<int:user_id>', methods=['GET'])
+def user_activity(user_id):
+	""" User last login/activity informaiton """
+	user = User.query.filter_by(id=user_id).first()
+	if not user:
+		return json_response('There is no users with this id', 400)
+	data = {
+		"success": True,
+		"last_login": user.last_login,
+		"last_activity": user.last_activity
+	}
+	return jsonify(data)
