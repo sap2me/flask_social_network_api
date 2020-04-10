40f1671327760e2ba937595ef18605377971881e
sap2me <sapronov.andrey.k@gmail.com>
2020-04-10 15:32:01 +0300

    added create_post

--- social_network/app/api.py
+++ social_network/app/api.py
@@ -1,8 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 
 from app import db
 from app.models import User, Post
 from app.tools import auth_required
+
+import datetime
 
 api = Blueprint('api', __name__)
 
@@ -13,4 +15,16 @@ def main():
 @api.route('/post', methods=['POST'])
 @auth_required
 def create_post(user):
-	return 'Here'
+	data = request.get_json()
+	if not data:
+		return jsonify({'error': 'No post data'})
+	text = data.get('text')
+	if not text:
+		return jsonify({'error': 'Text required'})
+	creation_time = datetime.datetime.utcnow()
+	new_post = Post(text=text, creation_time=creation_time, likes_amount=0, author=user)
+
+	db.session.add(new_post)
+	db.session.commit()
+
+	return jsonify({'message': "Post created"})
