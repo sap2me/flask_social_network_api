5460535e87fd0d8f90354735ac85a3c8812feb05
sap2me <sapronov.andrey.k@gmail.com>
2020-04-10 13:41:07 +0300

    updated db models

--- social_network/app/models.py
+++ social_network/app/models.py
@@ -1,21 +1,30 @@
 from app import db
 
+likes = db.Table('likes',
+		db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+		db.Column('post_id', db.Integer, db.ForeignKey('posts.id'))
+	)
 
 class User(db.Model):
-
+	__tablename__ = 'users'
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(64), unique=True)
 	password = db.Column(db.String(128))
+	last_login = db.Column(db.DateTime, nullable=True)
+	last_activity = db.Column(db.DateTime)
 	posts = db.relationship('Post', backref='author')
 
 	def __repr__(self):
 		return f"<User {self.name}>"
 
 class Post(db.Model):
+	__tablename__ = 'posts'
 	id = db.Column(db.Integer, primary_key=True)
 	text = db.Column(db.String(500))
 	creation_time = db.Column(db.DateTime)
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	likes_amount = db.Column(db.Integer, nullable=True)
+	likes = db.relationship('User', nullable=True, secondary=likes)
 
 	def __repr__(self):
 		return f"<Post {self.id}>"
--- social_network/config.py
+++ social_network/config.py
@@ -1,3 +1,4 @@
 class Config:
 	SECRET_KEY = "3b2518fc775bfac95246a77c"
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
 	DEBUG = True
