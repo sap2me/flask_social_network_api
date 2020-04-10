0886f70299659062bd4912656e20de819df8f432
sap2me <sapronov.andrey.k@gmail.com>
2020-04-10 16:59:15 +0300

    bug fix

--- social_network/app/models.py
+++ social_network/app/models.py
@@ -1,6 +1,7 @@
 from app import db
 
 likes = db.Table('likes',
+		db.Column('id', db.Integer, primary_key=True),
 		db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
 		db.Column('post_id', db.Integer, db.ForeignKey('posts.id'))
 	)
@@ -13,6 +14,7 @@ class User(db.Model):
 	last_login = db.Column(db.DateTime, nullable=True)
 	last_activity = db.Column(db.DateTime)
 	posts = db.relationship('Post', backref='author')
+	post_likes = db.relationship('Post', secondary=likes, backref='user')
 
 	def __repr__(self):
 		return f"<User {self.name}>"
@@ -24,7 +26,7 @@ class Post(db.Model):
 	creation_time = db.Column(db.DateTime)
 	user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 	likes_amount = db.Column(db.Integer, nullable=True)
-	likes = db.relationship('User', secondary=likes)
+	user_likes = db.relationship('User', secondary=likes, backref='post')
 
 	def __repr__(self):
 		return f"<Post {self.id}>"
