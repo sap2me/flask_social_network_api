99d6a601ef5815d8a97d6dc4803112bf500b8663
sap2me <sapronov.andrey.k@gmail.com>
2020-04-10 17:54:04 +0300

    refectoring

--- social_network/app/api.py
+++ social_network/app/api.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify
 
 from app import db
 from app.models import User, Post
-from app.tools import auth_required
+from app.tools import auth_required, json_response
 
 import datetime
 
--- social_network/app/views.py
+++ social_network/app/views.py
@@ -6,6 +6,7 @@ import datetime
 
 from app import db
 from app.models import User, Post
+from tools import json_response
 
 auth = Blueprint('auth', __name__)
 
@@ -13,19 +14,19 @@ auth = Blueprint('auth', __name__)
 def singup():
 	data = request.get_json()
 	if not data:
-		return jsonify({'error': 'No post data'})
+		return json_response('Data required', 400)
 	name = data.get('name')
 	password = data.get('password')
 
 	if not name or not password:
-		return jsonify({'error': 'Name and password required'}), 400
+		return json_response('Name and password required', 400)
 	
 	user = User.query.filter_by(name=name).first()
 	if user:
-		return jsonify({'error': 'This username is already taken'}), 400
+		return json_response('This username is already taken', 400)
 	
 	if len(password) < 7:
-		return jsonify({'error': 'Password must be at least 7 characters'}), 400
+		return json_response('Password must be at least 7 characters', 400)
 	
 	creation_time = datetime.datetime.utcnow()
 	hashed_password = generate_password_hash(password, method='sha256')
@@ -34,22 +35,22 @@ def singup():
 	db.session.add(new_user)
 	db.session.commit()
 
-	return jsonify({'message': 'User created'})
+	return json_response('User created')
 
 @auth.route('/login', methods=['POST'])
 def login():
 	data = request.get_json()
 	if not data:
-		return jsonify({'error': 'No post data'})
+		return json_response('Data required', 400)
 	name = data.get('name')
 	hashed_password= data.get('password')
 
 	if not name or not hashed_password:
-		return jsonify({'error': 'Name or password is incorrect'}), 401
+		return json_response('Name or password is incorrect', 401)
 
 	user = User.query.filter_by(name=name).first()
 	if not user or not check_password_hash(user.password, hashed_password):
-		return jsonify({'error': 'Name or password is incorrect'}), 401
+		return json_response('Name or password is incorrect', 401)
 
 	exp_time = datetime.datetime.utcnow() + datetime.timedelta(days=7)
 	auth_token = jwt.encode({'name': name, 'exp': exp_time}, current_app.config['SECRET_KEY'])
@@ -57,4 +58,4 @@ def login():
 
 	db.session.commit()
 
-	return jsonify({'jwt': auth_token.decode('utf-8')})
+	return jsonify({'jwt': auth_token.decode('utf-8'), 'success': True})
